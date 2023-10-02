449c79ece7c558576493fe095200aabd30c63fd2
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-10-02 14:42:58 +0300

    Update views.py

--- robots/views.py
+++ robots/views.py
@@ -1,8 +1,10 @@
 from django.http import HttpResponse
 from django.views.decorators.csrf import csrf_exempt
 from django.core.exceptions import ValidationError
+import openpyxl
 
 from .forms import RobotForm
+from .models import Robot
 
 
 @csrf_exempt
@@ -22,3 +24,23 @@ def add_robot(request):
             return HttpResponse(str(e), status=400)
     else:
         return HttpResponse('Invalid request method', status=405)
+
+
+def export_report(request):
+    robots = Robot.objects.filter(created__gte=datetime.now()-timedelta(days=7)).values('model', 'version').annotate(count=Count('id'))
+    models = robots.values('model').annotate(count=Count('id')
+                                             
+    wb = openpyxl.Workbook()
+
+    for model in models:
+        ws = wb.create_sheet(title=robot['model'])
+        ws.append(['Модель', 'Версия', 'Количество за неделю'])
+        data = robots.filter(model=model.model)
+        for row in data:
+            ws.append([row['model'], row['version'], row['count']])
+
+    response = HttpResponse(content_type='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+    response['Content-Disposition'] = 'attachment; filename="robots.xlsx"'
+    wb.save(response)
+
+    return response
