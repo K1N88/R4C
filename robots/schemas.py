942cfa57071930080b053433f3a8258c79fde4d5
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-10-02 15:51:03 +0300

    Delete robots/schemas.py

--- robots/schemas.py
+++
@@ -1,9 +0,0 @@
-robot_schema = {
-    "type": "object",
-    "properties": {
-        "model": {"type": "string"},
-        "version": {"type": "string"},
-        "created": {"type": "string", "format": "date-time"}
-    },
-    "required": ["model", "version", "created"]
-}
