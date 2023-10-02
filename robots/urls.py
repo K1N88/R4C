51f6720583003d21575359d926a413cb122faf90
Constantine Nazarov <105746796+K1N88@users.noreply.github.com>
2023-10-02 14:26:42 +0300

    Update urls.py

--- robots/urls.py
+++ robots/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import add_robot
+from .views import add_robot, week_report
 
 
 urlpatterns = [
-    path('add_robot/', add_robot),
+    path('add_robot/', add_robot, name='add_robot'),
+    path('week_report/', week_report, name='week_report'),
 ]
